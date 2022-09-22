bcac6ac9ff6421b8cb7597d90c7f2dfab75136d7
Nikita Gerasimoski <Nikita.Gerasimoski@netcetera.com>
2022-09-22 14:33:18 +0200

    Added GRSecurity as a Swift Package.

--- Package.swift
+++ Package.swift
@@ -11,22 +11,19 @@ let package = Package(
     products: [
         .library(
             name: "GirdersSwift",
-            targets: ["GirdersSwift", "GRSecurity"]),
+            targets: ["GirdersSwift"]),
     ],
     dependencies: [
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", exact: "1.9.5"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", exact: "6.17.1"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", exact: "4.2.2"),
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", exact: "9.0.0")
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs.git", exact: "9.0.0"),
+        .package(url: "https://github.com/NikitaGerasimoski/GRSecurity.git", branch:"main"),
     ],
     targets: [
         .target(
             name: "GirdersSwift",
-            dependencies: ["SwiftyBeaver", "PromiseKit", "KeychainAccess"]),
-        .binaryTarget(
-            name: "GRSecurity",
-            path: "Sources/GRSecurity.xcframework"
-        ),
+            dependencies: ["SwiftyBeaver", "PromiseKit", "KeychainAccess","GRSecurity"]),
         .testTarget(
             name: "GirdersSwiftTests",
             dependencies: [
